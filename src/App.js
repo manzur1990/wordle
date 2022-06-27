b4ba1d0acfb738da4a0bc1bcdc2c640a23a8efe5
manzur1990 <manzur-1990@hotmail.com>
2022-06-27 17:56:21 -0600

    restructuring files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Board from './components/Board';
 import './App.css';
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <nav>
+                <h1>Wordle</h1>
+                <Board />
+            </nav>
+        </div>
+    );
 }
 
 export default App;
