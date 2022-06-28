b55b9ecd893d9b37a0b339317d9cb1b99a4010ca
manzur1990 <manzur-1990@hotmail.com>
2022-06-27 18:44:27 -0600

    added context for letters state in board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Board from './components/Board';
 import './App.css';
+import Board from './components/Board';
+import { boardDefualt } from './Words';
+import Keyboard from './components/Keyboard';
+import { useState, createContext } from 'react';
+
+export const AppContext = createContext();
 
 function App() {
+    const [board, setBoard] = useState(boardDefualt);
     return (
         <div className='App'>
             <nav>
                 <h1>Wordle</h1>
-                <Board />
             </nav>
+            <AppContext.Provider value={{ board, setBoard }}>
+                <Board />
+                <Keyboard />
+            </AppContext.Provider>
         </div>
     );
 }
