eaac5fc07153b41e30e9582a35f31e5ea2c4d9b2
manzur1990 <manzur-1990@hotmail.com>
2022-06-28 21:01:07 -0600

    added handler to select letters

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import { AppContext } from '../App';
+import React, { useContext } from 'react';
 
 function Key({ keyVal, bigKey }) {
+    const selectLetter = () => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { board, setBoard } = useContext(AppContext);
+        const newBoard = [...board];
+        newBoard[0][0] = keyVal;
+        setBoard(newBoard);
+    };
     return (
-        <div className='key' id={bigKey && 'big'}>
+        <div className='key' id={bigKey && 'big'} onClick={selectLetter}>
             {keyVal}
         </div>
     );
