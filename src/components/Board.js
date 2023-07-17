e06f07a86286927b721dcf08fa75d8b3977bbb35
manzur1990 <manzur-1990@hotmail.com>
2023-07-17 10:28:11 -0600

    Board component has been refactored with DRY principle

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,54 +1,33 @@
-import React from 'react';
-import Letter from './Letter';
+import React from "react";
+import Letter from "./Letter";
 
 function Board() {
-    return (
-        <div className='board'>
-            {' '}
-            <div className='row'>
-                <Letter letterPos={0} attemptVal={0} />
-                <Letter letterPos={1} attemptVal={0} />
-                <Letter letterPos={2} attemptVal={0} />
-                <Letter letterPos={3} attemptVal={0} />
-                <Letter letterPos={4} attemptVal={0} />
-            </div>
-            <div className='row'>
-                <Letter letterPos={0} attemptVal={1} />
-                <Letter letterPos={1} attemptVal={1} />
-                <Letter letterPos={2} attemptVal={1} />
-                <Letter letterPos={3} attemptVal={1} />
-                <Letter letterPos={4} attemptVal={1} />
-            </div>
-            <div className='row'>
-                <Letter letterPos={0} attemptVal={2} />
-                <Letter letterPos={1} attemptVal={2} />
-                <Letter letterPos={2} attemptVal={2} />
-                <Letter letterPos={3} attemptVal={2} />
-                <Letter letterPos={4} attemptVal={2} />
-            </div>
-            <div className='row'>
-                <Letter letterPos={0} attemptVal={3} />
-                <Letter letterPos={1} attemptVal={3} />
-                <Letter letterPos={2} attemptVal={3} />
-                <Letter letterPos={3} attemptVal={3} />
-                <Letter letterPos={4} attemptVal={3} />
-            </div>
-            <div className='row'>
-                <Letter letterPos={0} attemptVal={4} />
-                <Letter letterPos={1} attemptVal={4} />
-                <Letter letterPos={2} attemptVal={4} />
-                <Letter letterPos={3} attemptVal={4} />
-                <Letter letterPos={4} attemptVal={4} />
-            </div>
-            <div className='row'>
-                <Letter letterPos={0} attemptVal={5} />
-                <Letter letterPos={1} attemptVal={5} />
-                <Letter letterPos={2} attemptVal={5} />
-                <Letter letterPos={3} attemptVal={5} />
-                <Letter letterPos={4} attemptVal={5} />
-            </div>
-        </div>
-    );
+  const numRows = 6;
+  const numLetters = 5;
+
+  const renderRows = () => {
+    const rows = [];
+
+    for (let row = 0; row < numRows; row++) {
+      const letters = [];
+
+      for (let letter = 0; letter < numLetters; letter++) {
+        const key = row * numLetters + letter;
+
+        letters.push(<Letter key={key} letterPos={letter} attemptVal={row} />);
+      }
+
+      rows.push(
+        <div className="row" key={row}>
+          {letters}
+        </div>,
+      );
+    }
+
+    return rows;
+  };
+
+  return <div className="board">{renderRows()}</div>;
 }
 
 export default Board;
